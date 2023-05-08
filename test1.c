9943516c45c0664339a624bbdc527d8dbe4b3e30
nguyentruonglongphi <132898308+nguyentruonglongphi@users.noreply.github.com>
2023-05-08 17:58:45 +0700

    print the n varialbe

--- test1.c
+++ test1.c
@@ -6,7 +6,7 @@ int main(){
         printf("vui long nhap N la so nguyen duong: ");
         scanf("%d", &n);
     } while (n<=0);
-    
+    printf("N vua nhap la: %d, n);
     printf ("oki la!");
     return 0;
 }
