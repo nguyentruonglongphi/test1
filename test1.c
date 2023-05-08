ab720a67087a24b785014b055f86d678f9a4dd80
nguyentruonglongphi <phi.nguyen-long@banvien.com.vn>
2023-05-08 17:56:12 +0700

    add the variale

--- test1.c
+++ test1.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(){
+    int n;
+    do
+    {
+        printf("vui long nhap N la so nguyen duong: ");
+        scanf("%d", &n);
+    } while (n<=0);
+    
     printf ("oki la!");
     return 0;
 }
